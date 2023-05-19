acf2b456ca21830f570c85199003feec204b8131
Denys1802 <78175281+Denys1802@users.noreply.github.com>
2023-05-19 21:22:49 +0000

    Deploying to gh-pages from @ Denys1802/test-tweets@100f711771d863382a4623fabcc18ac85930ff10 🚀

--- static/js/229.031f7836.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_tweets=self.webpackChunktest_tweets||[]).push([[229],{229:function(e,t,s){s.r(t);var n=s(184);t.default=function(){return(0,n.jsx)("div",{children:"Home"})}}}]);
-//# sourceMappingURL=229.031f7836.chunk.js.map
--- static/js/539.2c05e330.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_tweets=self.webpackChunktest_tweets||[]).push([[539],{539:function(e,t,s){s.r(t);var n=s(184);t.default=function(){return(0,n.jsx)("div",{children:"Card"})}}}]);
-//# sourceMappingURL=539.2c05e330.chunk.js.map
